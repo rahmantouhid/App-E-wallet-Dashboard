eafe0b5e55324bf815e08a21f759b0c40c31cfdb
Rahman Touhid <rahmantouhid07@gmail.com>
2022-06-05 21:31:09 +0600

    Adding Files

---
+++ orders.js
@@ -0,0 +1,35 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '888890',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '888890',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '888890',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '888890',
+        paymentStatus: 'Paid',
+        shipping: 'Delevered'
+    },
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '888890',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
